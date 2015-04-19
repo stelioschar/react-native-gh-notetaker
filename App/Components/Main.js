769a75a6ac75c6ed697e0f8c622e6f2e72d20e28
ijones16 <ijones16@github.com>
2015-04-18 19:28:46 -0700

    added loading indicator and user not found

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -96,6 +96,9 @@ class Main extends React.Component{
       });
   }
   render() {
+    var showErr = (
+      this.state.error ? <Text> {this.state.error} </Text> : <View></View>
+    );
     return(
       <View style={styles.mainContainer}>
       <Text style={styles.title}> Search for a Github User </Text>
@@ -109,6 +112,11 @@ class Main extends React.Component{
           underlayColor="white">
           <Text style={styles.buttonText}> SEARCH </Text>
         </TouchableHighlight>
+        <ActivityIndicatorIOS
+          animating={this.state.isLoading}
+          color="#111"
+          size="Large"></ActivityIndicatorIOS>
+        {showErr}
       </View>
       )
   }
