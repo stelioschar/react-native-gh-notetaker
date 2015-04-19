7214cb49723ff117baa7073d7f82a234dd6fd94b
ijones16 <ijones16@github.com>
2015-04-18 23:28:36 -0700

    goToRepos is hooked up with Repositories.js

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -1,5 +1,7 @@
 var React = require('react-native');
 var Profile = require('./Profile');
+var Repositories = require('./Repositories')
+var api = require('../Utils/api');
 
 
 var {
@@ -50,7 +52,17 @@ class Dashboard extends React.Component{
     })
   }
   goToRepos(){
-    console.log('Going to Repos');
+    api.getRepos(this.props.userInfo.login)
+      .then((res) => {
+        this.props.navigator.push({
+          component: Repositories,
+          title: 'Repos Page',
+          passProps: {
+            userInfo: this.props.userInfo,
+            repos: res
+          }
+        });
+    });
   }
   goToNotes(){
   console.log('Going to Notes');
