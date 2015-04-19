f4010e223a45e22aa8b81b5e00e354da6d607ec3
ijones16 <ijones16@github.com>
2015-04-18 19:53:19 -0700

    buttons added to dashboard

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -3,7 +3,9 @@ var React = require('react-native');
 var {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Image,
+  TouchableHighlight
 } = React;
 
 var styles = StyleSheet.create({
@@ -22,11 +24,53 @@ var styles = StyleSheet.create({
 });
 
 class Dashboard extends React.Component{
+   makeBackground(btn){
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+    if(btn === 0){
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1){
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
+  }
+  goToProfile(){
+    console.log('Going to Profile Page');
+  }
+  goToRepos(){
+    console.log('Going to Repos');
+  }
+  goToNotes(){
+  console.log('Going to Notes');
+  }
 render(){
   return (
       <View style={styles.container}>
-        <Text> This is the dashboard </Text>
-        <Text> {this.props.userInfo} </Text>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image}/>
+        <TouchableHighlight
+            style={this.makeBackground(0)}
+            onPress={this.goToProfile.bind(this)}
+            underlayColor="#88D4F5">
+              <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(1)}
+            onPress={this.goToRepos.bind(this)}
+            underlayColor="#E39EBF">
+              <Text style={styles.buttonText}>View Repositories</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(2)}
+            onPress={this.goToNotes.bind(this)}
+            underlayColor="#9BAAF3">
+              <Text style={styles.buttonText}>Take Notes</Text>
+        </TouchableHighlight>
       </View>
     )
 }
