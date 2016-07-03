caa77b05f768dc6e44e9850152d35bab5b4de49e
Ian Jones <ian@egghead.io>
2016-07-03 13:38:21 -0700

    update react-native to 0.19 :clap: (#11)

--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -3,66 +3,66 @@ var Badge = require('./Badge');
 
 
 var {
-  Text,
-  View,
-  StyleSheet,
-  ScrollView
-} = React;
+    Text,
+    View,
+    StyleSheet,
+    ScrollView
+    } = React;
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    alignSelf: 'center'
-  },
-  rowContainer: {
-    padding: 10
-  },
-  rowTitle: {
-    color: '#48BBEC',
-    fontSize: 16
-  },
-  rowContent: {
-    fontSize: 19
-  }
+    container: {
+        flex: 1
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
+    rowContainer: {
+        padding: 10
+    },
+    rowTitle: {
+        color: '#48BBEC',
+        fontSize: 16
+    },
+    rowContent: {
+        fontSize: 19
+    }
 });
 
 class Profile extends React.Component{
-  getRowTitle(user, item){
-    item = (item === 'public_repos') ? item.replace('_', ' ') : item;
-    return item[0] ? item[0].toUpperCase() + item.slice(1) : item;
-  }
-  render(){
-    var userInfo = this.props.userInfo;
-    var topicArr = ['company', 'location', 'followers', 'following', 'email', 'bio', 'public_repos'];
-    var list = topicArr.map((item, index) => {
-      if(!userInfo[item]){
-        return <View key={index}/>
-      } else {
+    getRowTitle(user, item){
+        item = (item === 'public_repos') ? item.replace('_', ' ') : item;
+        return item[0] ? item[0].toUpperCase() + item.slice(1) : item;
+    }
+    render(){
+        var userInfo = this.props.userInfo;
+        var topicArr = ['company', 'location', 'followers', 'following', 'email', 'bio', 'public_repos'];
+        var list = topicArr.map((item, index) => {
+            if(!userInfo[item]){
+                return <View key={index}/>
+            } else {
+                return (
+                    <View key={index}>
+                        <View style={styles.rowContainer}>
+                            <Text style={styles.rowTitle}> {this.getRowTitle(userInfo, item)} </Text>
+                            <Text style={styles.rowContent}> {userInfo[item]} </Text>
+                        </View>
+                    </View>
+                )
+            }
+        });
         return (
-          <View key={index}>
-            <View style={styles.rowContainer}>
-              <Text style={styles.rowTitle}> {this.getRowTitle(userInfo, item)} </Text>
-              <Text style={styles.rowContent}> {userInfo[item]} </Text>
-            </View>
-          </View>
+            <ScrollView style={styles.container}>
+                <Badge userInfo={this.props.userInfo}/>
+                {list}
+            </ScrollView>
         )
-      }
-    });
-    return (
-      <ScrollView style={styles.container}>
-        <Badge userInfo={this.props.userInfo}/>
-        {list}
-      </ScrollView>
-    )
-  }
+    }
 };
 
 Profile.propTypes = {
-  userInfo: React.PropTypes.object.isRequired
+    userInfo: React.PropTypes.object.isRequired
 }
 
 module.exports = Profile;
