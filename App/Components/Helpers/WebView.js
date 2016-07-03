90d5fed6b825993c0cac6dd281c80fe7ebd8196b
Ian Jones <ian@egghead.io>
2016-07-03 13:38:48 -0700

    updated react-native to 0.19 :100: (#16)

--- App/Components/Helpers/WebView.js
+++ App/Components/Helpers/WebView.js
@@ -1,31 +1,31 @@
 var React = require('react-native');
 
 var {
-  View,
-  WebView,
-  StyleSheet
-} = React;
+    View,
+    WebView,
+    StyleSheet
+    } = React;
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F6F6EF',
-    flexDirection: 'column',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F6F6EF',
+        flexDirection: 'column',
+    },
 });
 
 class Web extends React.Component{
-  render() {
-    return (
-      <View style={styles.container}>
-        <WebView url={this.props.url}/>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <WebView url={this.props.url}/>
+            </View>
+        );
+    }
 };
 
 Web.propTypes = {
- url: React.PropTypes.string.isRequired
+    url: React.PropTypes.string.isRequired
 };
 
 module.exports = Web;
